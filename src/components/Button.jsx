79212b9e93910e4b25eb3e0de410c02cad985dc1
miguelsabino23 <miguel2012palhada@gmail.com>
2024-09-21 09:16:19 -0300

    all commits

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+function Button(props) {
+  return (
+    <button className='bg-slate-400 p-2 rounded-md text-white' {...props}>
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+function Input(props) {
+  return (
+    <input
+      className='border border-slate-300 outline-slate-400 px-4 py-2 rounded-md'
+      {...props}
+    />
+  );
+}
+
+export default Input;
